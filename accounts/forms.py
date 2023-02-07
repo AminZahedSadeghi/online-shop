767e48185f9b432b46093d0fb400ab70da5f041b
amin <aminzahedsadeghi@gmail.com>
2023-02-07 21:50:31 +0330

    add UserLoginView

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib.auth import get_user_model
 
@@ -12,3 +13,8 @@ class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = get_user_model()
         fields = ('email', 'username',)
+
+
+class UserLoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
-# Create your views here.
+from .forms import UserLoginForm
+
+
+class UserLoginView(View):
+    form_class = UserLoginForm
+
+    def setup(self, request, *args, **kwargs):
+        self.next = request.GET.get('next')
+        return super().setup(request, *args, **kwargs)
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('pages:home')
+        return super().dispatch(request, *args, **kwargs)
+
+    def get(self, request):
+        form = self.form_class()
+        return render(request, 'accounts/login.html', {'form': form})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(username=cd['username'], password=cd['password'])
+            if user is not None:
+                login(request, user)
+                messages.success(request, 'شما با موفقیت وارد اکانت خود شدید :)', 'success')
+                return redirect(self.next)
+            else:
+                messages.error(request, 'همچین اکانتی با این مشصخات یافت نشد :/', 'danger')
+                return redirect('accounts:login')
+        else:
+            return render(request, 'accounts/login.html', {'form': form})
