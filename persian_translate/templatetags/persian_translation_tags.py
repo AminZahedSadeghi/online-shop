48b0f2af3be2503262ec4288befcb7c24105095f
amin <aminzahedsadeghi@gmail.com>
2023-03-11 16:59:07 +0330

    add translate_number filter to all of the numbers

--- persian_translate/templatetags/persian_translation_tags.py
+++ persian_translate/templatetags/persian_translation_tags.py
@@ -1,12 +1,18 @@
 from django import template
+from config.settings import LANGUAGE_CODE
 
 register = template.Library()
 
 @register.filter
 def translate_number(value):
+    value = str(value)
+    
     persian = '۰۱۲۳۴۵۶۷۸۹'
     english = '0123456789' 
+
+    if LANGUAGE_CODE == 'fa-ir':
+        engligh_to_persian_table = value.maketrans(english, persian)
+        return value.translate(engligh_to_persian_table)
     
-    value = str(value)
-    engligh_to_persian_table = value.maketrans(english, persian)
-    return value.translate(engligh_to_persian_table)
+    elif LANGUAGE_CODE == 'en-us':
+        return value
